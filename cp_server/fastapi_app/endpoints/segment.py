406afc6e27fc60123deac3cc5b8e0cacb81144fd
Benoit Roux <benoit.roux@gmail.com>
2025-02-18 17:49:01 +0100

    Add debug logging for image processing in segment_task

--- cp_server/fastapi_app/endpoints/segment.py
+++ cp_server/fastapi_app/endpoints/segment.py
@@ -19,7 +19,7 @@ def segment_task(request: Request, payload: PayLoadSegement) -> dict:
     directory = Path(payload.directory)
     
     logger.info(f"Starting image processing task from {directory}")
-    
+    logger.debug(f"{list(directory.rglob('*.tif'))=}")
     for img_file in directory.rglob("*.tif"):
         # Check that parent dir has the same name as the src_folder
         if img_file.parent.name != payload.src_folder:
@@ -28,7 +28,7 @@ def segment_task(request: Request, payload: PayLoadSegement) -> dict:
         # Ignore non-key_label images
         if payload.key_label not in img_file.name:
             continue
-        
+        logger.debug(f"Processing image: {img_file}")
         # Execute celery's task
         celery_app.send_task("cp_server.tasks_server.celery_tasks.process_images", kwargs={
             "settings": payload.settings,
